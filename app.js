048b9fb9c35fa8b08260ac32560c59c9fbfbf682
oskarcokl <oskar.cokl@gmail.com>
2026-03-06 21:14:55 +0100

    feat: Add frontend and backend error paths.

--- app.js
+++ app.js
@@ -1,13 +1,62 @@
 import express from "express";
 
+const ELASTIC_API_KEY="a0NxVndad0JwVkoxdHhGbFdDQXI6TG1LZW9heVNNODF3cE0wdjFyRkotQQ==";
+const ELASTIC_ENDPOINT="http://localhost:9200/app-logs/_doc"
+
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Error logging example');
 });
+
+app.post('/action', (req, res) => {
+    const body = req.body
+
+    if (!body.error) {
+        // Happy path
+        fetch(ELASTIC_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify({
+                action: "/action backend",
+                status: "success",
+                requestId: body.requestId,
+                correlationId: body.correlationId
+            }),
+            headers: {
+                "Authorization": `ApiKey ${ELASTIC_API_KEY}`,
+                "Content-Type": "application/json"
+            }
+        })
+
+        res.send('Success');
+        return;
+    } else {
+        fetch(ELASTIC_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify({
+                action: "/action backend",
+                status: "error",
+                requestId: body.requestId,
+                correlationId: body.correlationId,
+                trace: {
+                    errorMessage: "Error when processing data",
+                    errorCode: "DATA_PROCESSING_ERROR",
+                    functionName: "/action handler",
+                }
+            }),
+            headers: {
+                "Authorization": `ApiKey ${ELASTIC_API_KEY}`,
+                "Content-Type": "application/json"
+            }
+        })
+        res.status(500).send('Error in /action handler');
+        return;
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- public/index.js
+++ public/index.js
@@ -2,18 +2,23 @@
 const ELASTIC_API_KEY="a0NxVndad0JwVkoxdHhGbFdDQXI6TG1LZW9heVNNODF3cE0wdjFyRkotQQ==";
 const ELASTIC_ENDPOINT="http://localhost:9200/app-logs/_doc"
 
+const BACKEND_ENDPOINT = "http://localhost:3000/action"
+
 const actionButton = document.getElementById("action-button");
 const errorFrontendButton = document.getElementById("error-frontend-button");
 const errorBackendButton = document.getElementById("error-backend-button");
+
+const errorMessage = document.getElementById("error-msg");
 
 const correlationId = self.crypto.randomUUID();
 const requestId = self.crypto.randomUUID();
 
 actionButton.addEventListener("click", () => {
+    // Log action to elastic search
     fetch(ELASTIC_ENDPOINT, {
         method: "POST",
         body: JSON.stringify({
-            action: "button click",
+            action: "button click frontend",
             status: "success",
             requestId: requestId,
             correlationId: correlationId
@@ -23,12 +28,75 @@ actionButton.addEventListener("click", () => {
             "Content-Type": "application/json"
         }
     })
+
+    // Perform backend call
+    fetch(BACKEND_ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify({
+            requestId: requestId,
+            correlationId: correlationId
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
 });
 
 errorFrontendButton.addEventListener("click", () => {
-    console.log("Error frontend button");
+    // Log action to elastic search
+    fetch(ELASTIC_ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify({
+            action: "button click frontend",
+            status: "error",
+            requestId: requestId,
+            correlationId: correlationId,
+            trace: {
+                errorMessage: "Error when user clicked button",
+                errorCode: "USER_BUTTON_CLICK_ERROR",
+                buttonClicked: errorFrontendButton.id,
+            }
+        }),
+        headers: {
+            "Authorization": `ApiKey ${ELASTIC_API_KEY}`,
+            "Content-Type": "application/json"
+        }
+    })
+
+    // show correlationId to user so that he can search it
+    errorMessage.innerText = `Error when clicking button errorId: ${correlationId}`;
 });
 
-errorBackendButton.addEventListener("click", () => {
-    console.log("Error backend button");
+errorBackendButton.addEventListener("click", async () => {
+    // Log action to elastic search
+    fetch(ELASTIC_ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify({
+            action: "button click frontend",
+            status: "success",
+            requestId: requestId,
+            correlationId: correlationId
+        }),
+        headers: {
+            "Authorization": `ApiKey ${ELASTIC_API_KEY}`,
+            "Content-Type": "application/json"
+        }
+    })
+
+    // Perform backend call
+    const res = await fetch(BACKEND_ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify({
+            requestId: requestId,
+            correlationId: correlationId,
+            error: true,
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (res.status === 500) {
+        errorMessage.innerText = `Server error errorId: ${correlationId}`;
+    }
 });
